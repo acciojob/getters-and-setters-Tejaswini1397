bd8b91135e04f94d59c6c219dcc8a49f1c2da8b0
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2024-01-19 09:45:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -30,17 +30,7 @@ class Teacher extends Person {
   }
 }
 
-class Student extends Person {
-	study(){
-		console.log(`${this.name} is studying.`)
-	}
-}
 
-class Teacher extends Person {
-	 teach() {
-        console.log(`${this.name} is teaching.`);
-    }
-}
 
 // Do not change the code below this line
 window.Person = Person;
